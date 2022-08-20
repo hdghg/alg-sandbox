0f32c469028fa749c948c06938b4f00011d92771
Alexey Tokar <tokar.a.v@yandex.ru>
2022-08-20 11:13:39 +0300

    Refactor project to support modules

--- test.sh
+++ test.sh
@@ -5,18 +5,4 @@ cd build
 cmake ..
 cmake --build .
 
-for i in $(ls -1qd ../cases/*/)
-do
-  printf "Running test $i\n"
-
-  ./main < $i/input.txt > sol.txt
-  diff -Z sol.txt $i/output.txt > /dev/null
-  if test $? -gt 0
-  then
-    echo Test Failed
-    diff -Zy sol.txt $i/output.txt
-  else
-    echo Test success!
-  fi
-
-done
+../bubble/test.sh
